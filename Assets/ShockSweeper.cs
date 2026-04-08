4045138bfee4b1e2457e2db8ebf914b2b1c11f90
Justin Stransky <dva@colostate.edu>
2026-04-08 18:02:36 -0400

    Can move right

--- Assets/ShockSweeper.cs
+++ Assets/ShockSweeper.cs
@@ -8,6 +8,9 @@ public class ShockSweeper : MonoBehaviour
     Animator m_Animator;
     // Use this for deciding if the GameObject can jump or not
     bool m_Jump;
+    Rigidbody2D rb;
+    Vector2 direction;
+    bool isMoving = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +19,17 @@ public class ShockSweeper : MonoBehaviour
         m_Animator = gameObject.GetComponent<Animator>();
         // The GameObject cannot jump
         m_Jump = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         //Click the mouse or tap the screen to change the animation
+        if (Input.GetKeyDown("d")) {
+            isMoving = true;
+        }
         if (Input.GetKeyDown(KeyCode.Space))
             m_Jump = true;
 
@@ -35,5 +43,15 @@ public class ShockSweeper : MonoBehaviour
         //The GameObject is jumping, so send the Boolean as enabled to the Animator. The jump animation plays.
         if (m_Jump == true)
             m_Animator.SetBool("Slam", true);
+    }
+
+    // FixedUpdate is called to perform physics system calculations (e.g. movement)
+    void FixedUpdate()
+    {
+        if (isMoving) {
+            rb.linearVelocity = new Vector2(1, 0);
+        } else {
+            rb.linearVelocity = Vector2.zero;
+        }
     }
 }
